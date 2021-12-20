cf9c6b8a32d281424308b981da91d5d4b1b96a6d
khxlid256 <raihnkhalid@gmail.com>
2021-12-20 12:32:34 +0700

    Memmbuat API Verifikasi Login dan mengubah beberapa code API

--- api/daftar.php
+++ api/daftar.php
@@ -6,34 +6,28 @@ if (!$_POST) {
 header('Content-Type: application/json');
 include "./db.php";
 
-$username = $_POST['nama_pengguna'];
+$username = $_POST['nama_lengkap'];
 $password = password_hash($_POST['kata_sandi'], PASSWORD_DEFAULT);
 $email = $_POST['alamat_email'];
 $noTelp = $_POST['no_telepon'];
 
-$cekUsername = $db->prepare("SELECT * FROM user WHERE nama_pengguna= ?");
 $cekEmail = $db->prepare("SELECT * FROM user WHERE alamat_email= ?");
 $cekNoTelp = $db->prepare("SELECT * FROM user WHERE no_telepon= ?");
 
-$cekUsername->execute([$username]);
 $cekEmail->execute([$email]);
 $cekNoTelp->execute([$noTelp]);
 
-$cekRowUsername = $cekUsername->rowCount();
 $cekRowEmail = $cekEmail->rowCount();
 $cekRowNoTelp = $cekNoTelp->rowCount();
 
-if ($cekRowUsername > 0) {
-    echo "Nama Pengguna telah terdaftar!";
-    return FALSE; 
-} elseif ($cekRowEmail > 0) {
+if ($cekRowEmail > 0) {
     echo "Alamat Email telah terdaftar!";
     return FALSE;
 } elseif ($cekRowNoTelp > 0) {
     echo "No Telepon terlah terdaftar!";
     return FALSE;
 } else {
-$stmt = $db->prepare("INSERT INTO user (nama_pengguna, kata_sandi, alamat_email, no_telepon) VALUES (?, ?, ?, ?)");
+$stmt = $db->prepare("INSERT INTO user (nama_lengkap, kata_sandi, alamat_email, no_telepon) VALUES (?, ?, ?, ?)");
 $result = $stmt->execute([$username, $password, $email, $noTelp]);
 
 echo json_encode([
---
+++ api/login.php
@@ -0,0 +1,34 @@
+<?php
+if (!$_POST){
+    echo "Invalid Method";
+    return FALSE;
+} else {
+    
+header('Content-Type: application/json');
+include "./db.php";
+
+$email = $_POST['alamat_email'];
+$password = $_POST['kata_sandi'];
+
+$cekEmail = $db->prepare("SELECT * FROM user WHERE alamat_email = ? ");
+$cekEmail->execute([$email]);
+$cekRowEmail = $cekEmail->rowCount();
+
+if ($cekRowEmail < 1){
+    echo "Alamat Email tidak terdaftar!";
+    return FALSE;
+} else {
+    $cekPass = $cekEmail->fetch(PDO::FETCH_ASSOC);
+    if (password_verify($password, $cekPass['kata_sandi'])){
+        echo "Login Sukses";
+        return TRUE;
+    }else{
+        echo "Password yang anda masukkan salah!";
+        return FALSE;
+    }
+    
+}
+
+
+
+}
