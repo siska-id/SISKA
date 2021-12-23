e96fd4dbaff8773466640b8cb897d445fd274ba5
GwFirman <firmanzamzamiaziz@gmail.com>
2021-12-23 11:28:31 +0700

    Banner siska + Button menu 4x2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -25,34 +27,184 @@ class MyApp extends StatelessWidget {
                     Expanded(
                     child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Image.asset("assets/images/logo.png",width:100,height:200,fit: BoxFit.contain),
+                    child: Image.asset("assets/images/logo3.png",width:200,height:200,fit: BoxFit.cover),
                  ),
                  ),
                     Expanded(
                     child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child:  Text("Selamat datang di aplikasi SISKA",style: TextStyle(fontSize: 18,color: Colors. white,),),
+                    child:  Text("SELAMAT DATANG DI APLIKASI SISKA",style: TextStyle(fontSize: 17,color: Colors. white,fontWeight: FontWeight.bold),),
                  ),
                  ),
-                   
-                    
                   ],
                   ),
                ],
              ),
+            
             ),
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal:35,vertical:10 ),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                 Container(
+                   width: 60,
+                   height: 60,
+                   decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                  
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                   decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                    decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                    decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+               ],
+             ),
+             
+           ),
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal:35,vertical:25 ),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                 Container(
+                   width: 60,
+                   height: 60,
+                   decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                  
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                   decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                    decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+                  Container(
+                   width: 60,
+                   height: 60,
+                    decoration: BoxDecoration(
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(10),
+                     boxShadow: [
+                        BoxShadow(
+                         color: Colors.grey.withOpacity(0.5),
+                         spreadRadius: 2,
+                         blurRadius: 3,
+                         offset: Offset(0, 5), // changes position of shadow
+                      ),
+                    ],
+                   ),
+                 ),
+               ],
+             ),
+             
+           ),
           ], 
-          
         ),
+      
         
        
         bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels:false,
+          unselectedLabelStyle: TextStyle(
+            color: Colors.grey, fontFamily: 'Montserrat', fontSize: 12.0),
+          showUnselectedLabels: true,
           elevation: 0,
+          iconSize: 25,
+          
           items:[
           BottomNavigationBarItem (
-            label: "",
+            label: "Home",
             icon: Icon(
               Icons.home_outlined,
               color: Colors.grey,
