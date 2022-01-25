640aa05bb7241f7322bdfd35466475329ad0879f
baapw <bennyapfian22@gmail.com>
2022-01-25 14:51:05 +0700

    Menambahkan Profil

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import 'package:dropdown_search/dropdown_search.dart';
-=======
->>>>>>> benny/master
 import 'package:flutter/material.dart';
 import 'package:siskaproject/tautkan.dart';
 import 'layanan.dart';
 import 'tautkan.dart';
 import 'login.dart';
 import 'home.dart';
-<<<<<<< HEAD
-import 'profil.dart';
-
-void main() => runApp(ProfilPage());
-=======
 import 'buatakun.dart';
+
 void main() => runApp(BuatPage());
->>>>>>> benny/master
 
 class NavBar extends StatelessWidget {
   @override
@@ -23,10 +15,6 @@ class NavBar extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-<<<<<<< HEAD
-=======
-       
->>>>>>> benny/master
         bottomNavigationBar: BottomNavigationBar(
           unselectedItemColor: Colors.grey,
           selectedItemColor: Color.fromRGBO(51, 157, 255, 1),
