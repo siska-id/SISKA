2dd81e5c54aef31dd6a8a66b19503ac652d3917e
baapw <bennyapfian22@gmail.com>
2022-02-07 11:47:48 +0700

    profil page 90%

--- lib/profil.dart
+++ lib/profil.dart
@@ -10,7 +10,7 @@ class ProfilPage extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Color.fromRGBO(51, 157, 255, 1),
           body: Container(
-            padding: EdgeInsets.all(20.0),
+            padding: EdgeInsets.only(top: 70),
             child: Center(
                 child: Column(
               children: <Widget>[
@@ -18,6 +18,102 @@ class ProfilPage extends StatelessWidget {
                   backgroundColor: Colors.white,
                   backgroundImage: AssetImage("assets/images/avatar.png"),
                   radius: 70.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Nama",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.person_outlined),
+                    title: Text("Raihan Ganteng"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Email",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.email_outlined),
+                    title: Text("khlx123@gmail.com"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("No Telepon",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.phone_android_outlined),
+                    title: Text("+62xxxxxxxxxx"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Setting",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18)),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.settings_outlined),
+                    title: Text("Setting"),
+                  ),
                 )
               ],
             )),
